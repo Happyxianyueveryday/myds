16eaadfbefd043470e172576e4d6941dda6b2eb4
pzh <1972508489@qq.com>
2019-08-11 18:17:27 +0800

    Add files via upload

---
+++ Stack/LinkedStack/单元测试.cpp
@@ -0,0 +1,38 @@
+#include "LinkedStack.h"
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	LinkedStack sta1=LinkedStack();
+	LinkedStack sta2=LinkedStack();
+	
+	sta2.push(1);
+	sta2.push(2);
+	sta2.push(3);
+	sta2.push(4);
+	sta2.push(5);
+	sta2.push(6);
+	
+	int val;
+	cout<<sta2.top(val)<<endl;
+	cout<<val<<endl;
+	cout<<sta2.length()<<endl;
+	cout<<"----------------"<<endl;
+	
+	cout<<sta2.top(val)<<endl;
+	cout<<val<<endl;
+	cout<<"----------------"<<endl;
+	
+	LinkedStack sta3(sta1);
+	sta3=sta2;
+	
+	while(sta3.length())
+	{
+		sta3.pop(val);
+		cout<<val<<endl;
+	}
+	cout<<"----------------"<<endl;
+	
+}
