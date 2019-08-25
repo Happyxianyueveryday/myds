eafb4dce0a96e8a3396644f76a935753236142c8
pzh <1972508489@qq.com>
2019-08-25 20:48:30 +0800

    Add files via upload

---
+++ List/StaticList/单元测试.cpp
@@ -0,0 +1,29 @@
+#include "StaticList.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+      
+int main(void)
+{
+	StaticList list1(6);
+	list1.insert(0, 1); 
+	list1.insert(0, 2);
+	list1.insert(0, 4);
+	list1.insert(0, 5);
+	list1.insert(3, 3);
+	list1.insert(4, 6);
+	list1.insert(4, 6);
+	cout<<list1.toString()<<endl;
+	cout<<list1.full()<<endl;
+	int val;
+	list1.remove(4, val);
+	cout<<val<<endl;
+	cout<<list1.toString()<<endl;
+	list1.reverse();
+	cout<<list1.toString()<<endl;
+	list1.insert(2,12);
+	cout<<list1.toString()<<endl;
+	cout<<list1.middle()<<endl;
+}
+
