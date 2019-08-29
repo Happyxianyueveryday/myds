496b3113e06d3e45aa57b295d4f2cad24c272447
pzh <1972508489@qq.com>
2019-08-29 08:02:51 +0800

    Update HashElement.h

--- HashSet/ZipHashSet/HashElement.h
+++ HashSet/ZipHashSet/HashElement.h
@@ -14,7 +14,8 @@ class HashElement
     public:
     virtual int hashCode() const = 0;                               // 计算当前元素的哈希值 
     virtual bool operator== (const HashElement *other) const = 0;   // 相等运算符
-    virtual HashElement *clone(const HashElement *other);           // 虚拷贝函数，虚拷贝函数传入一个基类指针，要求返回一个深拷贝对象的基类指针，编写该方法时必须要进行向下转型 
+    virtual HashElement *clone(const HashElement *other) const = 0; // 虚拷贝函数，虚拷贝函数传入一个基类指针，要求返回一个深拷贝对象的基类指针，编写该方法时必须要进行向下转型 
+    virtual string toString() const = 0;                            // 字符串输出当前元素 
     virtual ~HashElement() {};                                      // 虚析构函数
 };
 
