27cdbff516d2d69f4eaa847088cc3572d00c0abd
pzh <1972508489@qq.com>
2019-08-31 21:23:45 +0800

    Add files via upload

---
+++ HashSet/ZipHashSet/HashSet.h
@@ -0,0 +1,27 @@
+#ifndef HASHSET_H
+#define HASHSET_h 
+#include "HashElement.h"    
+#include <iostream>  
+#include <string>
+#include <vector>
+
+using namespace std;
+
+/** 
+ * HashSet: 哈希集合接口抽象基类  
+ * note 1: 这里详细说明哈希表对于元素的复制策略，哈希表接口中的参数都使用了元素基类HashElement的指针，在实际插入时使用浅拷贝（出于性能考虑），资源的释放由HashSet来进行，返回列表时返回的也是浅拷贝（但是是底层const指针），因为对象拷贝需要大量时间 
+ * note 2: 只要使用指针参数的函数重写时都必须仔细判空，且注意NULL和nullptr的区别    
+ */
+class HashSet
+{    
+	public:    
+	virtual void insert(const HashElement *value) = 0 ;       // 向哈希表中插入指定值     
+	virtual void remove(const HashElement *value) = 0;        // 从哈希表中移除指定值    
+	virtual bool contain(const HashElement *value) const = 0; // 判断哈希表中是否包含指定值    
+	virtual vector<const HashElement *> values() const = 0;   // 获得哈希表中所有值的列表  
+	virtual int length() const = 0;                           // 获取哈希表的长度 
+	virtual string toString() const = 0;                      // 输出哈希表    
+	virtual ~HashSet() {}                                     // 哈希表抽象基类虚析构函数
+};
+
+#endif
---
+++ HashSet/ZipHashSet/单元测试.cpp
@@ -0,0 +1,107 @@
+#include "ZipHashSet.h"
+#include "TestElement.h"
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+ 
+int main(void)
+{
+	ZipHashSet hashset;
+	vector<HashElement *> dev;
+	for(int i=0;i<5;i++)
+	{
+		HashElement *temp=new TestElement(rand());
+		hashset.insert(temp);   // 一旦将对象加入哈希表，则该对象的释放是由哈希表负责的 
+		dev.push_back(temp);
+	} 
+	cout<<hashset.length()<<endl;
+	cout<<"hashset = "<<endl;
+	cout<<hashset.toString()<<endl;
+	
+	cout<<"--------------------------------------------"<<endl;
+	
+	for(int i=0;i<5;i++)
+	{
+		cout<<hashset.contain(dev[i])<<endl;
+	}
+	cout<<hashset.contain(new TestElement(2));
+	
+	cout<<"--------------------------------------------"<<endl;
+	
+	vector<const HashElement *> res=hashset.values();
+	for(int i=0;i<res.size();i++)
+	{
+		cout<<res[i]->toString()<<" ";
+	}
+	cout<<endl;
+	
+	cout<<"--------------------------------------------"<<endl;
+	
+	HashSet *hashset2=new ZipHashSet(dev);
+	cout<<"hashset2 = "<<endl;
+	cout<<hashset2->toString()<<endl;
+	
+	cout<<"--------------------------------------------"<<endl;
+	
+	ZipHashSet hashset3;
+	hashset3=hashset;
+	cout<<"hashset3 = "<<endl;
+	cout<<hashset3.toString()<<endl;
+	
+	cout<<"--------------------------------------------"<<endl;
+	
+	cout<<(hashset==hashset3)<<endl;
+	ZipHashSet hashset4=hashset3;
+	hashset4.remove(dev[0]);
+	cout<<(hashset4==hashset3)<<endl;
+	
+	cout<<"--------------------------------------------"<<endl;
+	
+	for(int i=0;i<5;i++)
+	{
+		hashset.remove(dev[i]);
+	}
+	hashset.remove(new TestElement(2));
+	cout<<hashset.length()<<endl;
+	cout<<"hashset = "<<endl;
+	cout<<hashset.toString()<<endl;
+	
+	cout<<"--------------------------------------------"<<endl;
+	cout<<"下面是扩容测试"<<endl;
+	
+	vector<HashElement *> data; 
+	for(int i=0;i<10;i++)    // 11个元素，未发生哈希扩容操作 
+	{
+		data.push_back(new TestElement(rand())); 
+	}
+	
+	ZipHashSet hashset5(data);
+	
+	cout<<hashset5.length()<<endl;
+	cout<<"hashset5 = "<<endl;
+	cout<<hashset5.toString()<<endl;
+	
+	for(int i=0;i<5;i++)    // 再随机插入5个整数，发生哈希扩容操作 
+	{
+		hashset5.insert(new TestElement(rand()));
+	}
+	
+	cout<<hashset5.length()<<endl;
+	cout<<"hashset5 = "<<endl; 
+	cout<<hashset5.toString()<<endl;
+	
+	for(int i=0;i<100;i++)   // 大范围随机测试 
+	{
+		hashset5.insert(new TestElement(rand()));
+	}
+	cout<<hashset5.length()<<endl;
+	cout<<"hashset5 = "<<endl; 
+	cout<<hashset5.toString()<<endl;
+	
+	cout<<"--------------------------------------------"<<endl;
+	ZipHashSet hashset6(move(hashset5));
+	cout<<hashset6.length()<<endl;
+	ZipHashSet hashset7=ZipHashSet()=move(hashset6);
+	cout<<hashset7.length()<<endl;
+	cout<<hashset6.length()<<endl;
+}
