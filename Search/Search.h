590615969a6a6e656f608b26d9045ba67fda2dad
pzh <1972508489@qq.com>
2019-09-08 02:49:43 +0800

    Add files via upload

---
+++ Search/Search.h
@@ -0,0 +1,59 @@
+#ifndef SEARCH_H      
+#define SEARCH_H
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+template<typename T> 
+class Search
+{
+	public:
+	static int linear_search(T data[], int size, T val);
+	static int binary_search(T data[], int size, T val); 
+};
+
+/**
+ * linear_search: 线性查找算法 
+ * param data: 待查找的列表 
+ * param size: 待查找列表的元素个数 
+ * param val: 查找的目标值
+ * return: 目标值所在的下标，若查找不到目标值则返回-1 
+ */
+template<typename T> 
+int Search<T>::linear_search(T data[], int size, T val)
+{
+	for(int i=0;i<size;i++)
+	{
+		if(data[i]==val)
+		return i;
+	}
+	return -1;
+}
+
+/**
+ * binary_search: 二分查找算法 
+ * param data: 待查找的有序列表 
+ * param size: 待查找列表的元素个数 
+ * param val: 查找的目标值
+ * return: 目标值所在的下标，若查找不到目标值则返回-1 
+ */
+template<typename T> 
+int Search<T>::binary_search(T data[], int size, T val)
+{
+	int begin=0, end=size-1;    // 当前查找片段的下标范围 
+	while(begin<=end)
+	{
+		int mid=(begin+end)/2;  // 计算中间元素的下标 
+		
+		if(val<data[mid])       // 若目标值小于中间元素，则在数组的左半部分查找 
+		end=mid-1;
+		else if(val>data[mid])  // 若目标值大于中间元素，则在数组的右半部分查找 
+		begin=mid+1;
+		else    // 目标值等于中间元素，直接返回下标mid 
+		return mid;
+	}
+	return -1;
+}
+
+#endif
---
+++ Search/单元测试.cpp
@@ -0,0 +1,32 @@
+#include "Search.h"      
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+	int data[]={1,3,5,7,9,11,13,15,17,19};
+	for(int i=0;i<10;i++)
+	{
+		cout<<Search<int>::binary_search(data, 10, data[i])<<" ";	
+	}
+	cout<<endl;
+	for(int i=0;i<10;i++) 
+	{
+		cout<<Search<int>::linear_search(data, 10, data[i])<<" ";	 
+	}
+	cout<<endl;
+	
+	int data1[]={2,4,6,8,10,12,14};
+	for(int i=0;i<7;i++)
+	{
+		cout<<Search<int>::binary_search(data1, 7, data1[i])<<" "; 	
+	}
+	cout<<endl;
+	for(int i=0;i<7;i++)
+	{
+		cout<<Search<int>::linear_search(data1, 7, data1[i])<<" ";	
+	}
+	cout<<endl;
+}
